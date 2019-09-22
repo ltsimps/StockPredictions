5cf221e9596beeec8d7d1df2a4d7f0d7e2413fdc
ltsimps <ltsimpso@me.com>
2019-09-22 09:33:52 -0400

    added Bulma

--- predictions/urls.py
+++ predictions/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import homePageView
+from .views import HomePageView
 
 
 
 urlpatterns = [
 
-       path('', homePageView, name='home')
+       path('', HomePageView.as_view(), name='home')
     ]
--- stock_predictions/settings.py
+++ stock_predictions/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'stock_predictions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')], #new
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
